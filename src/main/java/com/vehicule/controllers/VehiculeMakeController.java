c39a8ba5f51b95b118bb8fa8f36e9c84d780dfdc
MENGUEROMUALD <romylove19061993@gmail.com>
2020-09-15 14:49:59 +0100

    modification du service et controleur vehicule make

--- src/main/java/com/vehicule/controllers/VehiculeMakeController.java
+++ src/main/java/com/vehicule/controllers/VehiculeMakeController.java
@@ -1,13 +1,53 @@
 package com.vehicule.controllers;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.vehicule.models.VehiculeMake;
+import com.vehicule.services.VehiculeMakeService;
 
 @Controller
 public class VehiculeMakeController {
 
+	@Autowired private VehiculeMakeService vms;
+	
 	@GetMapping("/vehicule_make")
-	public String getVehiculesMakes() {
+	public String getVehiculesMakes(Model model) {
+		List<VehiculeMake> list_vm = vms.getVehiculeMake();
+		model.addAttribute("list_vehicule_make", list_vm);
 		return "vehicule_make";
+	}
+	
+	@PostMapping(name = "vehicule_make/addvm")
+	public String save(VehiculeMake vm) {
+		vms.saveVehiculeMake(vm);
+		return "redirect:/vehicule_make";
+	}
+	
+	@GetMapping(name = "vehicule_make/findById")
+	@ResponseBody
+	public Optional<VehiculeMake> getVehiculeMakeById(Integer id) {
+		return vms.getVehiculeMakeById(id);
+	}
+	
+	@RequestMapping(name = "vehicule_make/update", method = {RequestMethod.PUT,RequestMethod.GET})
+	public String update(VehiculeMake vm) {
+		vms.saveVehiculeMake(vm);
+		return "redirect:/vehicule_make";
+	}
+	
+	@RequestMapping(name = "vehicule_make/delete", method = {RequestMethod.DELETE,RequestMethod.GET})
+	public String delete(Integer id) {
+		vms.deleteById(id);
+		return "redirect:/vehicule_make";
 	}
 }
--- src/main/java/com/vehicule/services/VehiculeMakeService.java
+++ src/main/java/com/vehicule/services/VehiculeMakeService.java
@@ -1,8 +1,33 @@
 package com.vehicule.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+
+import com.vehicule.models.VehiculeMake;
+import com.vehicule.repository.VehiculeMakeRepository;
 
 @Service
 public class VehiculeMakeService {
 
+	@Autowired private VehiculeMakeRepository vmr;
+	
+	public List<VehiculeMake> getVehiculeMake() {
+		return vmr.findAll();
+	}
+	
+	public Optional<VehiculeMake> getVehiculeMakeById(Integer id){
+		return vmr.findById(id);
+	}
+	
+	public void saveVehiculeMake(VehiculeMake vm) {
+		vmr.save(vm);
+	}
+	
+	public void deleteById(Integer id) {
+		vmr.deleteById(id);
+	}
 }
