436430804538e1baa1eb9ebdce46ad1d5db39711
MENGUEROMUALD <romylove19061993@gmail.com>
2020-09-16 15:14:42 +0100

    Traitement du sous menu model de véhicule terminé

--- src/main/java/com/vehicule/controllers/VehiculeModelController.java
+++ src/main/java/com/vehicule/controllers/VehiculeModelController.java
@@ -1,13 +1,53 @@
 package com.vehicule.controllers;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.vehicule.models.VehiculeModel;
+import com.vehicule.services.VehiculeModelService;
 
 @Controller
 public class VehiculeModelController {
 
+	@Autowired private VehiculeModelService vms;
+	
 	@GetMapping("/vehicule_model")
-	public String getVehiculeModel() {
+	public String getVehiculeModel(Model model) {
+		List<VehiculeModel> vm = vms.listVehiculeModel();
+		model.addAttribute("list_vm", vm);
 		return "vehicule_model";
+	}
+	
+	@GetMapping(value = "vehicule_model/findById")
+	@ResponseBody
+	public Optional<VehiculeModel> getById(Integer id){
+		return vms.VehiculeModelById(id);
+	}
+	
+	@PostMapping(value = "vehicule_model/addvm")
+	public String save(VehiculeModel vm) {
+		vms.saveVm(vm);
+		return "redirect:/vehicule_model";
+	}
+	
+	@RequestMapping(value = "vehicule_model/update", method = {RequestMethod.PUT,RequestMethod.GET})
+	public String update(VehiculeModel vm) {
+		vms.saveVm(vm);
+		return "redirect:/vehicule_model";
+	}
+	
+	@RequestMapping(value = "vehicule_model/delete", method = {RequestMethod.DELETE,RequestMethod.GET})
+	public String deleteV(Integer id) {
+		vms.deleteVm(id);
+		return "redirect:/vehicule_model";
 	}
 }
--- src/main/java/com/vehicule/services/VehiculeModelService.java
+++ src/main/java/com/vehicule/services/VehiculeModelService.java
@@ -1,8 +1,32 @@
 package com.vehicule.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.vehicule.models.VehiculeModel;
+import com.vehicule.repository.VehiculeModelRepository;
 
 @Service
 public class VehiculeModelService {
 
+	@Autowired private VehiculeModelRepository vmr;
+	
+	public List<VehiculeModel> listVehiculeModel(){
+		return vmr.findAll();
+	}
+	
+	public Optional<VehiculeModel> VehiculeModelById(Integer id){
+		return vmr.findById(id);
+	}
+	
+	public void saveVm(VehiculeModel vm) {
+		vmr.save(vm);
+	}
+	
+	public void deleteVm(Integer id) {
+		vmr.deleteById(id); 
+	}
 }
