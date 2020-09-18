f395b2cbccf221d51637fc862e2f59c14936f37b
MENGUEROMUALD <romylove19061993@gmail.com>
2020-09-18 10:35:51 +0100

    traitement du service et controller du sous menu Invoice

--- src/main/java/com/vehicule/controllers/InvoiceController.java
+++ src/main/java/com/vehicule/controllers/InvoiceController.java
@@ -1,13 +1,66 @@
 package com.vehicule.controllers;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.vehicule.models.Client;
+import com.vehicule.models.Invoice;
+import com.vehicule.models.InvoiceStatus;
+import com.vehicule.services.ClientService;
+import com.vehicule.services.InvoiceService;
+import com.vehicule.services.InvoiceStatusService;
 
 @Controller
 public class InvoiceController {
 
+	@Autowired private InvoiceService invoiceService;
+	@Autowired private ClientService clienService;
+	@Autowired private InvoiceStatusService invoiceStatusService;
+	
 	@GetMapping("/invoice")
-	public String getInvoice() {
+	public String getInvoice(Model model) {
+		List<Invoice> list_invoice = invoiceService.listInvoice();
+		model.addAttribute("list_invoice", list_invoice);
+		
+		List<InvoiceStatus> list_instat = invoiceStatusService.listInvoiceStatut();
+		model.addAttribute("list_instat", list_instat);
+		
+		List<Client> list_client = clienService.getClient();
+		model.addAttribute("list_client", list_client);
+		
 		return "invoice";
+	}
+	
+	@GetMapping(value = "invoice/findById")
+	@ResponseBody
+	public Optional<Invoice> findById(Integer id){
+		return invoiceService.InvoiceById(id);
+	}
+	
+	@PostMapping(value = "invoice/add")
+	public String save(Invoice invoice) {
+		invoiceService.saveInvoice(invoice);
+		return "redirec:/invoice";
+	}
+	
+	@RequestMapping(value = "invoice/update", method = {RequestMethod.PUT, RequestMethod.GET})
+	public String update(Invoice invoice) {
+		invoiceService.saveInvoice(invoice);
+		return "redirec:/invoice";
+	}
+	
+	@RequestMapping(value = "invoice/delete", method = {RequestMethod.DELETE, RequestMethod.GET})
+	public String delete(Integer id) {
+		invoiceService.deleteInvoice(id);
+		return "redirec:/invoice";
 	}
 }
--- src/main/java/com/vehicule/services/InvoiceService.java
+++ src/main/java/com/vehicule/services/InvoiceService.java
@@ -1,8 +1,32 @@
 package com.vehicule.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.vehicule.models.Invoice;
+import com.vehicule.repository.InvoiceRepository;
 
 @Service
 public class InvoiceService {
-
+	
+	@Autowired private InvoiceRepository ir;
+	
+	public List<Invoice> listInvoice(){
+		return ir.findAll();
+	}
+	
+	public Optional<Invoice> InvoiceById(Integer id){
+		return ir.findById(id);
+	}
+	
+	public void saveInvoice(Invoice invoice) {
+		ir.save(invoice);
+	}
+	
+	public void deleteInvoice(Integer id) {
+		ir.deleteById(id);
+	}
 }
