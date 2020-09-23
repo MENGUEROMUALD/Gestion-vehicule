a4ca5040a4d643c7254b19f0af716410f0da3be0
MENGUEROMUALD <romylove19061993@gmail.com>
2020-09-23 17:12:21 +0100

    fin de traitement du service vehicule maintenance

--- src/main/java/com/vehicule/services/VehiculeMaintenanceService.java
+++ src/main/java/com/vehicule/services/VehiculeMaintenanceService.java
@@ -1,8 +1,32 @@
 package com.vehicule.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.vehicule.models.VehiculeMaintenance;
+import com.vehicule.repository.VehiculeMaintenanceRepository;
 
 @Service
 public class VehiculeMaintenanceService {
-
+	
+	@Autowired private VehiculeMaintenanceRepository vmr;
+	
+	public List<VehiculeMaintenance> listVehiculeMaintenance(){
+		return vmr.findAll();
+	}
+	
+	public Optional<VehiculeMaintenance> vmrById(Integer id){
+		return vmr.findById(id);
+	}
+	
+	public void saveVMR(VehiculeMaintenance vmrs) {
+		vmr.save(vmrs);
+	}
+	
+	public void deleteVMR(Integer id) {
+		vmr.deleteById(id);
+	}
 }
